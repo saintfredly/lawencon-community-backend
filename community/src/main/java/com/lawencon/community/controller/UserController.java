9362e19dafcd5b9a2d9e627eae4925ebbf7c1f1a
saintfredly <saintfredly69@gmail.com>
2023-03-08 22:34:07 +0700

    Create User Login

--- community/src/main/java/com/lawencon/community/controller/UserController.java
+++ community/src/main/java/com/lawencon/community/controller/UserController.java
@@ -1,5 +1,64 @@
 package com.lawencon.community.controller;
 
-public class UserController {
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lawencon.community.service.JWTService;
+import com.lawencon.community.service.UserService;
+import com.lawencon.community.pojo.login.PojoLoginReq;
+import com.lawencon.community.pojo.login.PojoLoginRes;
+import com.lawencon.community.model.User;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+	private final UserService userService;
+	private final JWTService jwtService;
+	private final AuthenticationManager authenticationManager;
+
+	public UserController(AuthenticationManager authenticationManager, UserService userService, JWTService jwtService) {
+		this.userService = userService;
+		this.jwtService = jwtService;
+		this.authenticationManager = authenticationManager;
+
+	}
+	
+	@PostMapping("login")
+	public ResponseEntity<?> login(@RequestBody final PojoLoginReq user) {
+		final Authentication auth = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPass());
+
+		authenticationManager.authenticate(auth);
+		final Optional<User> userOptional = userService.getByEmail(user.getEmail());
+
+		final Calendar cal = Calendar.getInstance();
+		cal.setTime(new Date());
+		cal.add(Calendar.HOUR_OF_DAY, 1);
+
+		final Map<String, Object> claims = new HashMap<>();
+		claims.put("exp", cal.getTime());
+		claims.put("id", userOptional.get().getId());
+
+		final PojoLoginRes loginRes = new PojoLoginRes();
+		loginRes.setToken(jwtService.generateJWT(claims));
+		loginRes.setUserId(userOptional.get().getId());
+		loginRes.setRoleCode(userOptional.get().getRole().getRoleCode());
+		if(loginRes.getFile()!=null) {
+			loginRes.setFile(userOptional.get().getFile().getId());			
+		}
+
+		return new ResponseEntity<>(loginRes, HttpStatus.OK);
+	}
 }
--- community/src/main/java/com/lawencon/community/service/UserService.java
+++ community/src/main/java/com/lawencon/community/service/UserService.java
@@ -1,5 +1,36 @@
 package com.lawencon.community.service;
 
-public class UserService {
+import java.util.ArrayList;
+import java.util.Optional;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.lawencon.community.dao.UserDao;
+import com.lawencon.community.model.User;
+
+@Service
+public class UserService implements UserDetailsService {
+	private final UserDao userDao;
+
+	public UserService(UserDao userDao) {
+		this.userDao = userDao;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		final Optional<User> user = userDao.getByEmail(username);
+
+		if (user.isPresent()) {
+			return new org.springframework.security.core.userdetails.User(username, user.get().getPasswords(),
+					new ArrayList<>());
+		}
+		throw new UsernameNotFoundException("Email and Password Not Match!");
+	}
+
+	public Optional<User> getByEmail(String email) {
+		return userDao.getByEmail(email);
+	}
 }
